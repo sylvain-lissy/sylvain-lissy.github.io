f377de94e97271bc3dfd4eab67bdbce25ee28013
NeeXuuS <66996706+sylvain-lissy@users.noreply.github.com>
2020-09-29 16:38:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-// var xmlhttp = new XMLHttpRequest();
-// var url = "data.json";
-
-// xmlhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         var myArr = JSON.parse(this.responseText);
-//         myFunction(myArr);
-//     }
-// };
-// xmlhttp.open("GET", url, true);
-// xmlhttp.send();
-
-// function myFunction(arr) {
-//     console.log(arr)
-//     // var out = "";
-//     // var i;
-//     // for(i = 0; i < arr.length; i++) {
-//     //     out += '<a href="' + arr[i].url + '">' +
-//     //     arr[i].display + '</a><br>';
-//     // }
-//     // document.getElementById("id01").innerHTML = out;
-// }
 fetch ("data.json")
-    .then(response => response.json())
-    .then(response =>{
-        console.log(response)
+    .then(oc => oc.json())
+    .then(oc =>{
+        console.log(oc)
     })
     .catch(error => console.error(error))
