eec4637aab12d1d170bb852257b3f2856080f112
NeeXuuS <66996706+sylvain-lissy@users.noreply.github.com>
2020-09-30 11:29:20 +0200

    Add files via upload

--- ocr.js
+++ ocr.js
@@ -2,12 +2,12 @@ fetch ("data.json")
     .then(oc => oc.json())
     .then(oc =>{
         console.log(oc)
-        const ocrHeader = document.getElementById("ocrHeader")
-        oc.forEach(ocr => {
+        const mainOcrHeader = document.getElementById("ocrHeader")
+        ocrHeader.forEach(ocr => {
             const h3OcrHeader = document.createElement("h3")
             h3OcrHeader.classList.add("card-title", "h5")
             h3OcrHeader.innerHTML = `${ocr.oc.id}`
-            ocrHeader.appendChild(h3OcrHeader)
+            mainOcrHeader.appendChild(h3OcrHeader)
         })
         console.log(oc.id)
     })
