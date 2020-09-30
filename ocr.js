3ae50eabdcbed10e4e2d13df3e59a44b78c0aed3
NeeXuuS <66996706+sylvain-lissy@users.noreply.github.com>
2020-09-30 11:55:20 +0200

    Add files via upload

--- ocr.js
+++ ocr.js
@@ -1,36 +1,7 @@
-// fetch ("data.json")
-//     .then(oc => oc.json())
-//     .then(oc =>{
-//         console.log(oc)
-//         const mainOcrHeader = document.getElementById("ocrHeader")
-//         ocrHeader.forEach(ocr => {
-//             const h3OcrHeader = document.createElement("h3")
-//             h3OcrHeader.classList.add("card-title", "h5")
-//             h3OcrHeader.innerHTML = `${ocr.oc.id}`
-//             mainOcrHeader.appendChild(h3OcrHeader)
-//         })
-//         console.log(oc.id)
-//     })
-//     //.catch(error => console.error(error))
-
 fetch("data.json")
-//Formatage reponse au format JSON
     .then(openclassrooms => openclassrooms.json())
-// Recuperation du JSON tableau des oursons //
     .then(openclassrooms => {
-        tableauOC(openclassrooms)
-})
-////////////////////////////////////////////////////// FUNCTIONS //////////////////////////////////////////////////////
-// Fonction de création du tableau des teddies
-function tableauOC(openclassrooms){
-    openclassrooms.forEach(oc => {
-    console.log(oc.id)
+        openclassrooms.forEach(oc => {
+            console.log(oc.id)
+        })
     })
-    //const mainOpenclassrooms = document.getElementById("ocrHeader")
-    // openclassrooms.forEach(ocrProjet => {
-    //     const h3Openclassrooms = document.createElement("h3")
-    //     h3Openclassrooms.classList.add("card-title", "h5")
-    //     h3Openclassrooms.innerHTML = `${ocrProjet.id}`
-    //     mainOpenclassrooms.appendChild(h3Openclassrooms)
-    // })
-}
