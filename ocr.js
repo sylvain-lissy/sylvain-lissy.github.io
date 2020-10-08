5d7ca2460ed312ede083f72ea5ed6e8b0ff557d8
NeeXuuS <66996706+sylvain-lissy@users.noreply.github.com>
2020-10-08 18:08:46 +0200

    Add files via upload

--- ocr.js
+++ ocr.js
@@ -8,32 +8,32 @@ fetch("ocr.json")
 function tableauProjets(openclassrooms){
     const mainProjet = document.getElementById("listeProjets")
     openclassrooms.forEach(projet => {
-        pId= projet.id
+        pId= projet.projetId
         const divProjet = document.createElement("div")
         divProjet.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
         divProjet.innerHTML = `            
         <div class="card mb-4 mb-lg-0 border-dark shadow h-100">
         <div class="card-header">
-            <h3 class="card-title h5">#OC&#x5c;P${projet.id}&#x5c;${projet.title} <span class="h6 badge badge-${projet.class}">${projet.status}</span></h3>
+            <h3 class="card-title h5">#OC&#x5c;P${projet.projetId}&#x5c;${projet.title} <span class="h6 badge badge-${projet.class}">${projet.status}</span></h3>
             <h4 class="card-subtitle h6 text-muted">${projet.text}</h4>
         </div>
         <div class="card-body">
             <p class="h6 card-text">Compétences mises en avant:</p>
-            <ul id="work${projet.id}">
+            <ul id="work${projet.projetId}">
             </ul>
             <p class="h6">Ressources supplémentaires:</p>
-            <ul id="more${projet.id}">
+            <ul id="more${projet.projetId}">
             </ul> 
-            <div class="row m-0 p-0 justify-content-center" id="lang${projet.id}">
+            <div class="row m-0 p-0 justify-content-center" id="lang${projet.projetId}">
             </div>                           
         </div>
         <div class="card-footer">
             <div class="row">
                 <div class="col-6">
-                    <a href="${projet.link[0]}" class="btn btn-primary btn-sm btn-block">Projet sur Git</a> 
+                    <a href="${projet.link[0]}" class="btn btn-primary btn-sm btn-block" target="_BLANK">Projet sur Git</a> 
                 </div>
                 <div class="col-6">
-                    <a href="${projet.link[1]}" class="btn btn-primary btn-sm btn-block">Page du site</a>
+                    <a href="${projet.link[1]}" class="btn btn-primary btn-sm btn-block target="_BLANK"">Page du site</a>
                 </div>
             </div>
         </div>`
@@ -51,7 +51,7 @@ function tableauProjets(openclassrooms){
             const listing = document.getElementById("more"+pId)
             const liWork = document.createElement("li")
             liWork.classList.add("small")
-            liWork.innerHTML = `<a href="${projet.more[i].link}" class="text-primary ">${projet.more[i].text}</a> `
+            liWork.innerHTML = `<a href="${projet.more[i].link}" class="text-primary " target="_BLANK">${projet.more[i].text}</a> `
             listing.appendChild(liWork)
         }
         //Fonction langages/framework
